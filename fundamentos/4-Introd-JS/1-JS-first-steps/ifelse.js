2c41b5205f3847b45e305425202c34eeeff17f87
Leonardo Zanette <leonardobzanette@gmail.com>
2022-04-06 15:56:10 -0300

    exercicios videos aula

---
+++ fundamentos/4-Introd-JS/1-JS-first-steps/ifelse.js
@@ -0,0 +1,11 @@
+let nota = 85
+
+if (nota >= 80) {
+    console.log("Aprovada");
+}
+else if (nota >= 60) {
+    console.log("Espera");
+}
+else {
+    console.log("Reprovado")
+}
---
+++ fundamentos/4-Introd-JS/1-JS-first-steps/tipos-aritm.js
@@ -0,0 +1,15 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+console.log(typeof patientAge);
+const base = 5;
+let height = 8;
+const area = base * height
+const perimeter = base + height + base + height;
+
+console.log(area);
+console.log(perimeter);
