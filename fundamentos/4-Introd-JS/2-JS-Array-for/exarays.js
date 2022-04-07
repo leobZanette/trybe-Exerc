d028e6a2df2e97fb5aa0c57b619c32e437e2a583
Leonardo Zanette <leonardobzanette@gmail.com>
2022-04-07 15:18:44 -0300

    exercicios antes aula ao vivo  4.2

---
+++ fundamentos/4-Introd-JS/2-JS-Array-for/exarays.js
@@ -0,0 +1,8 @@
+let menu = ['home','serviços','portfolio','link'];
+let menuServico = menu[1];
+let indexPortifolio = menu.indexOf('portfolio');
+menu.push('contato');
+
+console.log(menuServico);
+console.log(indexPortifolio);
+console.log(menu);
---
+++ fundamentos/4-Introd-JS/2-JS-Array-for/exfor.js
@@ -0,0 +1,11 @@
+let groceryList = ['arro','feijao','alface','melancia'];
+
+for ( let i = 0; i < groceryList.length; i += 1 ) {
+    console.log(groceryList[i]);
+}
+
+let names = ['joao','maria','antonio','margarida'];
+
+for ( let nome of names) {
+    console.log(nome);
+}
